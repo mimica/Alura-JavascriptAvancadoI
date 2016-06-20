16b51dc9cb746c37fc2cf5d0120330f762b0ca4b
mimica <jmimica@gmail.com>
2016-06-20 19:04:32 -0300

    Update chapter 1

--- aluraframe/client/js/index.js
+++ aluraframe/client/js/index.js
@@ -5,3 +5,35 @@ var campos = [
 ];
 
 console.log(campos);
+
+var tbody = document.querySelector('table tbody');
+
+document.querySelector('.form').addEventListener('submit', function(event) {
+
+	event.preventDefault();
+
+	//alert('oi');
+	var tr = document.createElement('tr');
+
+	campos.forEach(function(campo) {
+		var td = document.createElement('td');
+		td.textContent = campo.value;
+		tr.appendChild(td);
+	});
+
+	var tdVolume = document.createElement('td');
+
+	// quantidade * valor
+	tdVolume.textContent = campos[1].value * campos[2].value;
+	tr.appendChild(tdVolume);
+
+	tbody.appendChild(tr);
+
+	// Reset form fields and set data field focus
+	campos[0].value = '';
+	campos[1].value = '1';
+	campos[2].value = '0';
+
+	campos[0].focus();
+
+});
