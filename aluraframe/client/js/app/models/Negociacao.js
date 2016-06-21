a43c977ee67196534005d1c042618b53d972c1b4
mimica <jmimica@gmail.com>
2016-06-20 21:28:15 -0300

    Update chapter 2.

---
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -0,0 +1,29 @@
+//ES6
+
+class Negociacao {
+
+	constructor(data, quantidade, valor) {
+		this._data = new Date(data.getTime()); // defensive programming
+		this._quantidade = quantidade;
+		this._valor = valor;
+		Object.freeze(this);
+	}
+
+	get volume() {
+		return this._quantidade * this._valor;
+	}
+
+	get data() {
+		return new Date(this._data.getTime()); // defensive programming
+	}
+
+	get quantidade() {
+		return this._quantidade;
+	}
+
+	get valor() {
+		return this._valor;
+	}
+
+
+}
