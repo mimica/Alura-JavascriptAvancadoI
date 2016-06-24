5e785b5815cab0b3de9bb343516748a34e2ed7da
mimica <jmimica@gmail.com>
2016-06-24 19:16:02 -0300

    Update chapter 3.

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -3,47 +3,21 @@ class NegociacaoController {
 	constructor() {
 
 		let $ = document.querySelector.bind(document);
+		
 		this._inputData = $('#data');
 		this._inputQuantidade = $('#quantidade');
 		this._inputValor = $('#valor');
 		
-		// Poderia ser declarado depois
-		this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+		
+		this._listaNegociacoes = new Bind(
+			new ListaNegociacoes(),
+			new NegociacoesView($('#negociacoesView')),
+			'adiciona', 'esvazia');
 
-
-		let self = this;
-		this._listaNegociacoes = new Proxy( new ListaNegociacoes(), {
-
-	        get(target, prop, receiver) {
-
-	         if (['adiciona', 'esvazia'].includes(prop) && typeof(target[prop]) == typeof(Function) ) {
-
-	            //
-	            return function() {
-
-	                console.log(`interceptando ${prop}`);
-	                Reflect.apply(target[prop], target, arguments);
-	                self._negociacoesView.update(target);
-	            }
-
-	         }
-
-	         return Reflect.get(target, prop, receiver);
-
-	        }
-    	});
-
-    
-
-
-		/*
-		// ???
-		this._listaNegociacoes = new ListaNegociacoes( model => this._negociacoesView.update(model) );
-		*/
-
-		this._mensagem = new Mensagem();
-		this._mensagemView = new MensagemView($('#mensagemView'));
-		this._mensagemView.update(this._mensagem);
+		this._mensagem = new Bind(
+			new Mensagem(),
+			new MensagemView($('#mensagemView')),
+			'texto');
 	}
 
 
@@ -54,7 +28,7 @@ class NegociacaoController {
 		this._listaNegociacoes.adiciona(this._criaNegociacao());
 
 		this._mensagem.texto = 'Negociação adicionada com sucesso';
-		this._mensagemView.update(this._mensagem);
+		//this._mensagemView.update(this._mensagem);
 		
 		this._limpaFormulario();
 	}
@@ -62,7 +36,7 @@ class NegociacaoController {
 	apaga() {
 		this._listaNegociacoes.esvazia();
 		this._mensagem.texto = 'Negociações apagas com sucesso.';
-		this._mensagemView.update(this._mensagem);
+		//this._mensagemView.update(this._mensagem);
 	}
 
 	_criaNegociacao() {
---
+++ aluraframe/client/js/app/helpers/Bind.js
@@ -0,0 +1,17 @@
+class Bind {
+
+	// ... REST operator
+	constructor(model, view, ...props) {
+
+		let proxy = ProxyFactory.create(
+			model,
+			props, 
+			(model) => view.update(model));
+
+		view.update(model);
+
+		return proxy;
+	}
+
+
+}
